52d83ab5b22e1c3cac92d6b32c61bd061831f497
Bruno Mulim <108417283+bmulim@users.noreply.github.com>
2026-05-23 17:05:00 -0300

    feat: update About page layout with profile image and section structure

--- apps/web/src/app/(public)/about/page.tsx
+++ apps/web/src/app/(public)/about/page.tsx
@@ -1,5 +1,14 @@
 export default function About() {
   return (
-    <main className="flex flex-col items-center justify-between ">About</main>
+    <main className="flex flex-col items-center justify-between p-12">
+      <section className="flex">
+        <article className="w-full flex items-center justify-start">
+          <h1 className="text-3xl font-bold text-left">Sobre mim</h1>
+        </article>
+        <div>
+          <Image src="/images/profile.jpg" alt="Foto de perfil" width={200} height={200} className="rounded-full" />
+        </div>
+      </section>
+    </main>
   );
 }
